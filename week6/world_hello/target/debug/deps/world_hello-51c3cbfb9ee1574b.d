f8e5e337304c0cdf985d12b376323cca4cfd0cdf
Dailiduzhou <qzhsdream@163.com>
2025-12-03 22:15:13 +0800

    feat: rusty noob

---
+++ week6/world_hello/target/debug/deps/world_hello-51c3cbfb9ee1574b.d
@@ -0,0 +1,5 @@
+/root/weeklyLearningJournal/weeklyLearningJournal/week6/world_hello/target/debug/deps/world_hello-51c3cbfb9ee1574b.d: src/main.rs
+
+/root/weeklyLearningJournal/weeklyLearningJournal/week6/world_hello/target/debug/deps/world_hello-51c3cbfb9ee1574b: src/main.rs
+
+src/main.rs:
